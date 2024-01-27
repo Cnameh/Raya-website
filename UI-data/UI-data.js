4bd418480190bf7e232b825eea4abe5c3a0b0ab5
Amirsina Mehrabi <mehrabisina020@gmail.com>
2024-01-27 16:18:20 +0330

    Added The Navigation Bar components
    added it in components folder, also base sass file was updated.

---
+++ UI-data/UI-data.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { name: "خانه", path: "/" },
+  { name: "بلاگ", path: "/blog" },
+  { name: "نمونه کار", path: "/portofolio" },
+  { name: "درباره ما", path: "/about-us" },
+  { name: "تیم ما", path: "/our-team" },
+  { name: "تماس با ما", path: "/contact-us" },
+];
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,10 @@
 import "../styles/css/root.css";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />
     </>
   );
